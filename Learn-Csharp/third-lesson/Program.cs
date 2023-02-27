0b3f951a796ad38d4662af03c0f88444383e1cea
9AlexeyEmelyanov <9aleshaeboshu1@gmail.com>
2023-02-27 23:15:40 +0300

    add and finish 21 task

--- Learn-Csharp/third-lesson/Program.cs
+++ Learn-Csharp/third-lesson/Program.cs
@@ -37,7 +37,7 @@ void InputFunction(){
     }
 }
 
-InputFunction();
+//InputFunction();
 /*
 Задача 21
 
@@ -48,6 +48,43 @@ A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
 
 */
+
+void FillPoints(out int[] firstPoint, out int[] secondPoint){
+    //Координаты точки (x1,y1,z1)
+    Console.Write("Enter coordinates of x1 point >>> ");
+    int firstXPoint = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter coordinates of y1 point >>> ");
+    int firstYPoint = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter coordinates of z1 point >>> ");
+    int firtsZPoint = Convert.ToInt32(Console.ReadLine());
+
+    //Координаты точки (x2,y2,z2)
+    Console.Write("Enter coordinates of x2 point >>> ");
+    int secondXPoint = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter coordinates of y2 point >>> ");
+    int secondYPoint = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter coordinates of z2 point >>> ");
+    int secondZPoint = Convert.ToInt32(Console.ReadLine());
+    firstPoint= new int[]{firstXPoint, firstYPoint, firtsZPoint};
+    secondPoint= new int[]{secondXPoint, secondYPoint, secondZPoint};
+
+}
+
+double DistanceBetweenPoints(){
+    int[] firstPoint;
+    int[] secondPoint;
+    FillPoints(out firstPoint, out secondPoint);
+    return Math.Sqrt(Math.Pow(secondPoint[0] - firstPoint[0], 2) + 
+                Math.Pow(secondPoint[1] - firstPoint[1], 2) + 
+                    Math.Pow(secondPoint[1] - firstPoint[1], 2));
+
+}
+
+void Task21(){
+    Console.WriteLine(DistanceBetweenPoints());
+}
+
+Task21();
 
 /*
 Задача 23
